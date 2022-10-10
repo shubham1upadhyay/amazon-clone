faa81df69a342e0848d71123f957dbd286b2f03a
shubhamupadhyay <matra.upadhyay@gmail.com>
2022-10-10 18:34:23 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,192 @@
+function help(){
+    window.open("C:Users/Shubham/OneDrive/Desktop/amazon-clone/box11-data/conditions-of-use-&-sales.html")
+}
+function privacy(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/box11-data/privacy.html")
+}
+function interest(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/box11-data/interest-based-ad.html")
+}
+function australia(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/country-homepage/australia.html","_top")
+}
+function brazil(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/country-homepage/brazil.html","_top")
+}
+function canada(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/country-homepage/canada.html","_top")
+}
+function china(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/country-homepage/china.html","_top")
+}
+function france(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/country-homepage/france.html","_top")
+}
+function germany(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/country-homepage/germany.html","_top")
+}
+function italy(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/country-homepage/italy.html","_top")
+}
+function japan(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/country-homepage/japan.html","_top")
+}
+function mexico(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function netherlands(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function poland(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function singapore(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function spain(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function turkey(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function united_arab_emirates(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function united_kingdoms(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function united_states(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function about(){
+    window.open("C:/Users/Shubham/OneDrive/Desktop/amazon-clone/box8-data/about-us.html","_top")
+}
+function careers(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function amazon_cares(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function press_releases(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function gift(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function amazon_science(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function sell_on_amazon(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}function facebook(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function twitter(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function instagram(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}function sell_under(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function global_sell(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function affiliate(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function fulfilment(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function advertise(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function returns_centre(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function amazon_pay(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function covid19(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function your_account(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function amazon_app(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function amazon_cares(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function protection(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function amazon_assistant(){
+    var myArray=[];
+    myArray.push("The site is under maintenance.");
+    alert(myArray[0]);
+}
+function help(){
+    window.open("C:Users/Shubham/OneDrive/Desktop/amazon-clone/box11-data/conditions-of-use-&-sales.html")
+}
